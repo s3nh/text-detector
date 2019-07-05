750aededcfd43d41260df2afb0e222353c549117
s3nh <steam.panek@gmail.com>
2019-07-05 21:52:58 +0200

    Add CRNN architecture

---
+++ models/crnn/crnn.py
@@ -0,0 +1,105 @@
+import torch.nn as nn 
+
+
+class BidirectionalLSTM(nn.Module):
+
+    # in hidden out
+
+    def __init__(self, in, hidden, out):
+        super(BidirectionalLSTM, self).__init__
+
+
+
+        self.rnn = nn.LSTM(in, hidden, bidirectional=True)
+        self.embedding = nn.Linear(hidden * 2, out)
+
+
+
+    # Forward pass
+
+    def forward(self, x):
+
+        recurrent, _ = self.rnn(x)
+        T, b, h = recurrent.size()
+        t_rec = recurrent.view(T * b, h)
+
+
+        output = self.embedding(t_rec) 
+        output = output.view(T, b, -1)
+
+
+        return output
+
+
+# CRNN architecture 
+
+
+
+class CRNN(nn.Module):
+
+
+    def __init__(self, imgh, nc, nclass, nh, n_rnn=2, leakyReLU = False):
+
+
+
+        super(CRNN, self).__init__()
+        assert imgh % 16 == 0 " Assumption that image height must be an multiple of 16"
+
+        #FIXME
+
+
+
+
+		ks = [3, 3, 3, 3, 3, 3, 2]
+        ps = [1, 1, 1, 1, 1, 1, 0]
+        ss = [1, 1, 1, 1, 1, 1, 1]
+        nm = [64, 128, 256, 256, 512, 512, 512]
+
+        cnn = nn.Sequential()
+
+        def convRelu(i, batchNormalization=False):
+            nIn = nc if i == 0 else nm[i - 1]
+            nOut = nm[i]
+            cnn.add_module('conv{0}'.format(i),
+                           nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
+            if batchNormalization:
+                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
+            if leakyRelu:
+                cnn.add_module('relu{0}'.format(i),
+                               nn.LeakyReLU(0.2, inplace=True))
+            else:
+                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
+
+        convRelu(0)
+        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 64x16x64
+        convRelu(1)
+        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 128x8x32
+        convRelu(2, True)
+        convRelu(3)
+        cnn.add_module('pooling{0}'.format(2),
+                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 256x4x16
+        convRelu(4, True)
+        convRelu(5)
+        cnn.add_module('pooling{0}'.format(3),
+                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x2x16
+        convRelu(6, True)  # 512x1x16
+
+        self.cnn = cnn
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(512, nh, nh),
+            BidirectionalLSTM(nh, nh, nclass))
+
+    def forward(self, input):
+        # conv features
+        conv = self.cnn(input)
+        b, c, h, w = conv.size()
+        assert h == 1, "the height of conv must be 1"
+        conv = conv.squeeze(2)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+
+        # rnn features
+        output = self.rnn(conv)
+
+        return output
+
+
